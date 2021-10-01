ba400295936217fdca0310ee8c8d291cdca8fd33
rileygomez314 <rileygomez314@gmail.com>
2021-10-01 12:05:58 -0500

    hw4 w/ test cases

--- src/homework/04_repetition/dna.cpp
+++ src/homework/04_repetition/dna.cpp
@@ -1,3 +1,38 @@
 //add include statements
+#include "dna.h"
 
-//add function(s) code here
+//factorial function
+int factorial(int num)
+{
+    int sum=1;
+    while (num>0)
+    {
+        sum = sum * num;
+        num--;
+    }
+
+    return sum;
+}
+
+//greatest common divisor (gcd) function 
+
+int gcd(int num1, int num2)
+{
+    while (num1 != num2)
+    {
+        if (num1 < num2)
+        {
+            int num3 = num1;
+            num3 = num1;
+            num1 = num2;
+            num2 = num3;
+        }
+
+        if (num1 > num2)
+        {
+            num1 = num1 - num2;
+        }
+    }
+
+    return num1;
+}
--- src/homework/04_repetition/main.cpp
+++ src/homework/04_repetition/main.cpp
@@ -1,6 +1,10 @@
 //write include statements
+#include <iostream>
+#include "dna.h"
 
 //write using statements
+using std::cout; 
+using std::cin;
 
 
 /*
@@ -11,6 +15,58 @@ function and display the result. Program runs as long as
 user enters a y or Y.
 */
 int main() 
-{
-	return 0;
-}
+	{
+		
+		int choice; 
+		int num;
+		int num1;
+		int num2;
+
+		do
+		{
+			//display for main menu
+			cout << "\n";
+			cout << "What would you like to do?: \n";
+			cout << "1 - Calculate factorial \n";
+			cout << "2 - Calculate the greatest common divisor \n";
+			cout << "3 - Exit the program \n";
+			cout << "\n";
+
+			cout << "Choose an option: \n";
+			cin >> choice;
+
+			switch(choice)
+			{
+				case 1: 
+					cout<<"Please enter any integer";
+					cin >> num;
+					cout << "The result is: " << factorial(num);
+					break;
+
+				case 2: 
+					cout << "Please enter any integer \n";
+					cin >> num1;
+					cout << "Enter any other integer \n";
+					cin >> num2;
+					cout << "The greatest common divisor is: " << gcd(num1, num2);
+					break;
+
+				case 3: 
+					cout << "Are you sure you want to exit? (Yes = 0) \n";
+					cin >> choice;
+					break;
+
+				default:
+					cout << "Enter a valid choice number";
+					break;
+			}
+
+		}
+		while (choice != 0);
+		{
+			cout << "Exiting program...";
+		}
+
+
+		return 0;
+	}
