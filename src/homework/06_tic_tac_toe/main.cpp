a5956fdbdab1abb9630b9e5c80c7d95811bcbabc
rileygomez271 <riley.gomez@g.austincc.edu>
2021-10-22 22:36:20 -0500

    hw 6 with test cases

--- src/homework/06_tic_tac_toe/main.cpp
+++ src/homework/06_tic_tac_toe/main.cpp
@@ -1,6 +1,34 @@
-
+#include <iostream>
+#include <vector>
+#include "string"
+#include "tic_tac_toe.h"
+using std::string; using std::cout; using std::cin;
+using std::cin;
 int main() 
 {
-
+	std::string first_player;
+	int position;
+	int loop=0;
+	int repeat=1;
+	bool getout=false;
+	TicTacToe instance;
+	while (repeat==1)
+	{
+		cout<<"Press X or O for the player one: "<<"\n";
+		cin>>first_player;
+		instance.start_game(first_player);
+		instance.display_board();
+		while (getout!=true)
+		{
+			cout<<"Player: "<<instance.get_player()<<"'s turn, Select a space to fill from 1 to 9"<<"\n";
+			cin>>position;
+			instance.mark_board(position); 
+			instance.display_board();
+			getout=instance.game_over();
+		}
+		cout<<"Game over, press 1 to play again, 2 to exit "<<"\n";
+		cin>>repeat;
+	}
+	cout<<"End of program"<<"\n";
 	return 0;
 }
--- src/homework/06_tic_tac_toe/tic_tac_toe.h
+++ src/homework/06_tic_tac_toe/tic_tac_toe.h
@@ -1,1 +1,19 @@
-//h
+#include <string>
+#include <vector>
+using std::string;
+using std::vector;
+class TicTacToe
+{
+    public:
+        bool game_over();
+        void start_game(std::string first_player);
+        void mark_board(int position);
+        void display_board()const;
+        std::string get_player()const;
+    private:
+        std::string player;
+        std::vector<std::string> pegs= std::vector<std::string>(9," ");
+        void set_next_player();
+        bool check_board_full();
+        void clear_board();
+};
